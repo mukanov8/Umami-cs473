643e5f89cd429ef8d5ad3ba66797fc4d5eaf73a2
mukanov8 <mukanov.ayan@gmail.com>
2020-11-21 23:22:55 +0900

    added styled component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
-// import Paper from 'material-ui/core/Paper'
-
+import styled from 'styled-components';
 import Card from '@material-ui/core/Card';
+
+const FeedCard = styled(Card)`
+  ${'' /* padding-bottom: 10px; */}
+  margin: 0px 0px 20px 0px;
+  max-width: 500px;
+  max-height: 800px;
+`;
 
 const Feed = () => {
   const [posts, setPosts] = useState();
@@ -17,16 +23,14 @@ const Feed = () => {
 
   return (
     <div >
-
-    {/* <div> */}
       <h1>Feed</h1>
       {posts ? (
         posts.map((p, i) => (
-          <Card key={i}>
+          <FeedCard key={i}>
             <img src={p.video} alt="video" />
             <p>{p.caption}</p>
             <p>Exercise: {p.exercise}</p>
-          </Card>
+          </FeedCard>
         ))
       ) : (
         <></>
