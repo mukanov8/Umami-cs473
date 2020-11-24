7f85e07dd35812e27e8c3a6a16aadbea15cbae0c
Michael Tegegn <michaelwalelegn11@gmail.com>
2020-11-24 11:46:28 +0900

    Add personal posts to the profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import VideoPlayer from "./VideoPlayer";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import { CardHeader } from "@material-ui/core";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,10 +27,37 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     display: "flex",
   },
+  bigMargin: {
+    marginTop: 100,
+  },
+  smallMargin: {
+    marginTop: 30,
+    marginBottom: 10,
+  },
 }));
 
 const Profile = ({ user }) => {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .get()
+      .then((res) => {
+        const allPosts = res.docs.map((p) => p.data());
+
+        setPosts(allPosts.filter((p) => (p.userId = user.Id)));
+      });
+  }, []);
+
+  function displayCondition() {
+    if (posts === undefined) return false;
+    else {
+      return posts.length > 0;
+    }
+  }
   const classes = useStyles();
+
   return (
     <container className={classes.mainContainer}>
       <Avatar aria-label="recipe" className={classes.large}>
@@ -30,6 +65,53 @@ const Profile = ({ user }) => {
       </Avatar>
       <h3>{user.name}</h3>
       <h3>{user.exerciseGoal}</h3>
+      <div> </div>
+      <div className={classes.postsDisplay}>
+        <div className={classes.smallMargin}>Posts by {user.name} </div>
+      </div>
+
+      <Container>
+        {displayCondition() ? (
+          posts.map((p, i) => (
+            <Card className={classes.root} key={i}>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                    {p.userName[0].toUpperCase()}
+                  </Avatar>
+                }
+                action={
+                  <Typography
+                    className={classes.action}
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Exercise: {p.exercise}
+                  </Typography>
+                }
+                title={p.userName}
+                subheader={p.timestamp && p.timestamp.toDate().toString()}
+              />
+              {p.type.includes("video") ? (
+                <VideoPlayer videoUrl={p.video} />
+              ) : (
+                <CardMedia
+                  className={classes.media}
+                  image={p.video}
+                  title="Image/Video"
+                />
+              )}
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {p.caption}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <h2 className={classes.mainContainer}> No Posts yet</h2>
+        )}
+      </Container>
     </container>
   );
 };
